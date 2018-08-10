07699acd89ad96ecb4b4c30cc4869f25c20f6213
Eric Klimowich <eklimowich@hotmail.com>
2018-08-10 01:58:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function deleteFromObjectByKey(object, key)
 function destructivelyDeleteFromObjectByKey(object, key)
 {
   var newObj = Object.assign({}, object)
-  delete newObj.key
+  delete newObj[key]
   return newObj
 }
